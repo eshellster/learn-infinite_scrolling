9efadebe830e6e9ad2ae37f3c8aa99d0a51a6667
eshellster <eshellster@gmail.com>
2022-01-02 19:50:39 +0900

    fix: 주석 삭제

--- src/useBookSerach.tsx
+++ src/useBookSerach.tsx
@@ -15,15 +15,6 @@ export function UseBookSerach(query: string, pageNumber: number) {
     setLoading(true);
     setError(false);
 
-    /* fetch를 사용한 기본 쿼리
-    let q = `q=${query}`;
-    let page = `&page=${pageNumber}`;
-    fetch("http://openlibrary.org/search.json?" + q + page).then((res) => {
-      res.json().then((response) => {
-        console.log(response.docs);
-      });
-    });
-    //*/
     //* 강사가 제신한 방법으로  axios cancel로 비동기 요청 취소
     let cancel: Canceler;
 
@@ -31,7 +22,7 @@ export function UseBookSerach(query: string, pageNumber: number) {
       method: "GET",
       url: "http://openlibrary.org/search.json",
       params: { q: query, page: pageNumber },
-      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      cancelToken: new axios.CancelToken((c) => (cancel = c)), //
     })
       .then((res) => {
         setBooks((prevBooks) => {
@@ -42,7 +33,7 @@ export function UseBookSerach(query: string, pageNumber: number) {
             ]),
           ];
         });
-        setHasMore(res.data.docs.length > 0);
+        setHasMore(res.data.docs.length > 0); // 자료의 유무
         setLoading(false);
       })
       .catch((e) => {
@@ -50,24 +41,6 @@ export function UseBookSerach(query: string, pageNumber: number) {
         setError(true);
       });
     return () => cancel();
-    //*/
-    /* 최신 axios 방법
-
-    const CancelToken = axios.CancelToken;
-    let cancel: Canceler;
-    let q = `q=${query}`;
-    let page = `&page=${pageNumber}`;
-    axios.get(`http://openlibrary.org/search.json${q}${page}`, {
-      cancelToken: new CancelToken(function executor(c) {
-        // excutor 함수는 cancel 함수를 매개 변수로 받습니다.
-        cancel = c;
-      }),
-    });
-
-    // 요청 취소
-    cancel();
-
-    //*/
   }, [query, pageNumber]);
   return { loading, error, books, hasMore };
 }
